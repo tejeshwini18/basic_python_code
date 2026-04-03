53034f2085bafa73d66b880d6f21cb757d8d30b3
Tejeshwini Sahu <76488126+tejeshwini18@users.noreply.github.com>
2026-04-03 23:17:32 +0530

    Add files via upload

---
+++ basic_code/practice_problems/13._lru_cache.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+# Example
+lru = LRUCache(2)
+lru.put(1, 10)
+print(lru.get(1))
+lru.put(3, 30)
+print(lru.get(2))
