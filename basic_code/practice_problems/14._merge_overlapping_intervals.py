4504f72e2add57daba79139923d0a1077906da3a
Tejeshwini Sahu <76488126+tejeshwini18@users.noreply.github.com>
2026-04-04 20:41:27 +0530

    Add files via upload

---
+++ basic_code/practice_problems/14._merge_overlapping_intervals.py
@@ -0,0 +1,15 @@
+def merge_intervals(intervals):
+    if not intervals:
+        return []
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
+    for current in intervals[1:]:
+        last = merged[-1]
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+    return merged
+
+# Example
+print(merge_intervals([[1,3],[2,6],[8,10],[15,18]]))
